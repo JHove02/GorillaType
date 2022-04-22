aa0db5ddc1135261ecd1704e0046c80e84144315
benjaminrybacki <39493502+benjaminrybacki@users.noreply.github.com>
2022-04-21 23:38:43 -0500

    Prompt generation and length changing

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TestService } from '../test.service';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private testService: TestService) { }
+
+  promptLength: number = 10;
+  prompt: string = "";
 
   ngOnInit(): void {
+    this.getWords(this.promptLength);
   }
 
+  getWords(promptLength: number): void {
+    this.prompt = this.testService.givePrompt(promptLength);
+  }
+
+  onClick(element: any) {
+    const buttons = document.querySelectorAll('.toggle-length-button');
+    buttons.forEach(button => {
+      button.classList.remove('active');
+    });
+    element.classList.add('active');
+    if(element.classList.contains('10'))
+      this.promptLength = 10;
+    else if(element.classList.contains('25'))
+      this.promptLength = 25;
+    else
+      this.promptLength = 50;
+    this.getWords(this.promptLength);
+  }
 }
