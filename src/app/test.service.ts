f3839445fda7d5ccb9d519e1b38471a483f15da1
benjaminrybacki <39493502+benjaminrybacki@users.noreply.github.com>
2022-04-22 21:10:49 -0500

    Edited common words array

--- src/app/test.service.ts
+++ src/app/test.service.ts
@@ -8,7 +8,7 @@ export class TestService {
   constructor() { }
 
   WORDS: string[] = ["the", "of", "and", "a", "to", "in", "is", "you", "that", "it", "he", "was", "for", 
-  "on", "are", "as", "with", "his", "they", "I", "at", "be", "this", "have", "from", "or", "one", "had", 
+  "on", "are", "as", "with", "his", "they", "at", "be", "this", "have", "from", "or", "one", "had", 
   "by", "word", "but", "not", "what", "all", "were", "we", "when", "your", "can", "said", "there", "use", 
   "an", "each", "which", "she", "do", "how", "their", "if", "will", "up", "other", "about", "out", "many", 
   "then", "them", "these", "so", "some", "her", "would", "make", "like", "him", "into", "time", "has", "look", 
