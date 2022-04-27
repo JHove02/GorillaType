78e68f0b00dd05a710ec473494f117c9ae507892
Carson Bring <79489533+carsonbring@users.noreply.github.com>
2022-04-27 12:42:46 -0500

    getUser method

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { User } from './user';
 import { userInfo } from 'os';
+import {map} from 'rxjs/operators'
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +13,19 @@ export class UserService {
 
   addUser(newUser: User){
     return this.http.post('https://gorillatype-47b71-default-rtdb.firebaseio.com/'+ 'user.json', newUser);
+
   }
+  getUsers(){
+    return this.http.get<User[]>('https://gorillatype-47b71-default-rtdb.firebaseio.com/' + 'user.json')
+    .pipe(map(responseData => {
+      const userArray: User[] = [];
+      for(const key in responseData){
+        userArray.push(responseData[key]);
+      }
+      return userArray;
+    })
+    );
+  }
+  
 }
 
