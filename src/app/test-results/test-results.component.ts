8e9011a983ac21bac0e2e9561ea1dd1f9af79aaa
benjaminrybacki <39493502+benjaminrybacki@users.noreply.github.com>
2022-04-26 21:09:19 -0500

    start of results component display

--- src/app/test-results/test-results.component.ts
+++ src/app/test-results/test-results.component.ts
@@ -8,14 +8,27 @@ import { TestService } from '../test.service';
 })
 export class TestResultsComponent implements OnInit {
 
+  accuracy?: number;
+  time?: string;
+  incorrect?: number;
+  correct?: number;
+  finishedPrompt?: Element;
+
   constructor(private testService: TestService) { }
 
   ngOnInit(): void {
-    
+    this.displayResults();
   }
 
-  accuracy?: number;
-  time?: string;
+  displayResults(): void {
+    this.correct = this.testService.getCorrect();
+    this.incorrect = this.testService.getIncorrect();
+    this.accuracy = this.correct / (this.incorrect + this.correct);
+    this.time = this.testService.getTime();
+    this.finishedPrompt = this.testService.getFinishedPrompt();
+    document.querySelector('.info')?.appendChild(this.finishedPrompt);
+  }
+
 
 
 }
