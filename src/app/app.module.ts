f365e3cbfb2e7cbbc11a348d27841a289d77d6d3
benjaminrybacki <39493502+benjaminrybacki@users.noreply.github.com>
2022-04-26 19:42:53 -0500

    Fixed merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,16 @@ import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRouterModule } from './app-router.module';
-<<<<<<< HEAD
 import { TestResultsComponent } from './test-results/test-results.component';
-=======
 import { SettingsComponent } from './settings/settings.component';
->>>>>>> 8f1e9bb9bc7292c1fe430a02178f1429535ac323
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     HeaderComponent,
-<<<<<<< HEAD
-    TestResultsComponent
-=======
+    TestResultsComponent,
     SettingsComponent
->>>>>>> 8f1e9bb9bc7292c1fe430a02178f1429535ac323
   ],
   imports: [
     BrowserModule,
