4e59c95415600c195af4a92a93ac54e71f78a883
JHove02 <Jhove0302@gmail.com>
2022-04-26 21:10:59 -0500

    About and settings theme work

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -14,6 +14,8 @@ export class SettingsComponent implements OnInit {
   }
 
   defaultClick():void{
+    document.documentElement.style.setProperty('--button', '#BBB');
+    document.documentElement.style.setProperty('--incorrect-color', 'rgb(180, 85, 85)');
     document.documentElement.style.setProperty('--background-color', '#656565');
     document.documentElement.style.setProperty('--accent-color', 'rgb(62, 227, 172)');
     document.documentElement.style.setProperty('--header-background', '#444');
@@ -21,6 +23,8 @@ export class SettingsComponent implements OnInit {
   }
 
   redBlackClick():void{
+    document.documentElement.style.setProperty('--button', '#BBB');
+    document.documentElement.style.setProperty('--incorrect-color', '#656565');
     document.documentElement.style.setProperty('--background-color', '#1E1B18');
     document.documentElement.style.setProperty('--accent-color', '#D8315B');
     document.documentElement.style.setProperty('--header-background', '#2A2A2E');
@@ -28,10 +32,30 @@ export class SettingsComponent implements OnInit {
   }
 
   monkeyClick():void{
+    document.documentElement.style.setProperty('--button', '#BBB');
+    document.documentElement.style.setProperty('--incorrect-color', 'rgb(180, 85, 85)');
     document.documentElement.style.setProperty('--background-color', '#444');
     document.documentElement.style.setProperty('--accent-color', '#F8EE33');
     document.documentElement.style.setProperty('--header-background', '#444');
     document.documentElement.style.setProperty('--accent-light', '#FFFFF');
+  }
+
+  OrangeClick(): void{
+    document.documentElement.style.setProperty('--button', '#000000');
+    document.documentElement.style.setProperty('--incorrect-color', '#656565');
+    document.documentElement.style.setProperty('--background-color', '#FF5F1A');
+    document.documentElement.style.setProperty('--accent-color', '#FFFFFF');
+    document.documentElement.style.setProperty('--header-background', '#4C86A8');
+    document.documentElement.style.setProperty('--accent-light', '#FFFFFF');
+  }
+
+  blueClick():void{
+    document.documentElement.style.setProperty('--button', '#000000');
+    document.documentElement.style.setProperty('--incorrect-color', 'rgb(180, 85, 85)');
+    document.documentElement.style.setProperty('--background-color', '#0CBABA');
+    document.documentElement.style.setProperty('--accent-color', '#FFFFFF');
+    document.documentElement.style.setProperty('--header-background', '#234AA6');
+    document.documentElement.style.setProperty('--accent-light', '#000000');
   }
 
 }
