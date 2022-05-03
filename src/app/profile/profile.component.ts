5f64601fe06d092a1c85ced716552c28cbbbba2e
JHove02 <jhove0302@gmail.com>
2022-05-02 21:09:26 -0500

    Profile page css and added submit button for password change in settings

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,21 +8,21 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  private users: User[] =[];
+  private users: User[] = [];
   public newUsername: string = "";
   public newPassword: string = "";
   public id: string = "d";
   public createdUser: boolean = false;
-  public LogIn: boolean =true;
+  public LogIn: boolean = true;
   public CreateUser: boolean = false;
   public logUsername: string = "";
   public logPassword: string = "";
   public signedIn: boolean = false;
   public incorrectLogin: boolean = false;
-  public currentUser: User = {username:'',password:'', TenWPM : -1, TwentyFiveWPM: -1, FiftyWPM: -1};
-  constructor(private userServ:UserService) { }
-  fetchData(){
-    this.userServ.getUsers().subscribe(data =>{
+  public currentUser: User = { username: '', password: '', TenWPM: -1, TwentyFiveWPM: -1, FiftyWPM: -1 };
+  constructor(private userServ: UserService) { }
+  fetchData() {
+    this.userServ.getUsers().subscribe(data => {
       this.users = data;
       console.log(data);
     })
@@ -32,36 +32,36 @@ export class ProfileComponent implements OnInit {
     this.incorrectLogin = false;
     this.createdUser = false;
     this.fetchData();
-    
+
     this.id = this.userServ.getUserId();
-    
-    
+
+
   }
-  login(){
+  login() {
     let id: string = "";
-    this.userServ.logIn(this.logUsername,this.logPassword).subscribe(data =>{
+    this.userServ.logIn(this.logUsername, this.logPassword).subscribe(data => {
       id = data;
-      console.log('hello '+ data)
+      console.log('hello ' + data)
       this.userServ.setUserId(id);
       this.id = this.userServ.getUserId();
       this.isSignedIn();
-      if(!this.signedIn){
+      if (!this.signedIn) {
         this.incorrectLogin = true;
-      }else{
-        this.userServ.getCurrentUser().subscribe(data =>{
+      } else {
+        this.userServ.getCurrentUser().subscribe(data => {
           this.currentUser = data;
         });
       }
     });
-    
-    
-    
-    
-    
+
+
+
+
+
   }
 
-  addNewUser(){
-   
+  addNewUser() {
+
     const newUser: User = {
       username: this.newUsername,
       password: this.newPassword,
@@ -69,44 +69,44 @@ export class ProfileComponent implements OnInit {
       TwentyFiveWPM: 0,
       FiftyWPM: 0
     }
-    this.userServ.addUser(newUser).subscribe(data =>{
+    this.userServ.addUser(newUser).subscribe(data => {
       //THIS IS IMPORTANT VERY I MPORTANT
       let tempid = Object.values(data)[0];
-      console.log(tempid );
+      console.log(tempid);
       this.fetchData();
     })
     this.createdUser = true;
     this.LogIn = true;
     this.CreateUser = false;
   }
-  setLogin(){
+  setLogin() {
     this.LogIn = true;
     this.CreateUser = false;
   }
-  setCreateUser(){
+  setCreateUser() {
     this.CreateUser = true;
     this.LogIn = false;
   }
-  isSignedIn(){
-    let id: string= this.userServ.getUserId()
-    if(id == ""){
+  isSignedIn() {
+    let id: string = this.userServ.getUserId()
+    if (id == "") {
       this.signedIn = false;
       console.log('not signed in')
-    }else{
-      
+    } else {
+
       this.signedIn = true;
       this.createdUser = false;
-    this.LogIn = false;
-    this.CreateUser = false;
+      this.LogIn = false;
+      this.CreateUser = false;
     }
   }
-  deleteCarson(){
+  deleteCarson() {
     this.userServ.deleteUser("asdf");
   }
-  
 
 
 
-  
+
+
 
 }
