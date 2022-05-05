f9ada676acbcf8864a9e81d012c49d7c6c6d848f
benjaminrybacki <39493502+benjaminrybacki@users.noreply.github.com>
2022-05-04 20:35:35 -0500

    hover effects for theme buttons

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -15,6 +15,7 @@ export class SettingsComponent implements OnInit {
 
   defaultClick():void{
     document.documentElement.style.setProperty('--button', '#BBB');
+    document.documentElement.style.setProperty('--text-color', '#BBB');
     document.documentElement.style.setProperty('--incorrect-color', 'rgb(180, 85, 85)');
     document.documentElement.style.setProperty('--background-color', '#656565');
     document.documentElement.style.setProperty('--accent-color', 'rgb(62, 227, 172)');
@@ -24,6 +25,7 @@ export class SettingsComponent implements OnInit {
 
   redBlackClick():void{
     document.documentElement.style.setProperty('--button', '#BBB');
+    document.documentElement.style.setProperty('--text-color', '#BBB');
     document.documentElement.style.setProperty('--incorrect-color', '#656565');
     document.documentElement.style.setProperty('--background-color', '#1E1B18');
     document.documentElement.style.setProperty('--accent-color', '#D8315B');
@@ -33,37 +35,46 @@ export class SettingsComponent implements OnInit {
 
   monkeyClick():void{
     document.documentElement.style.setProperty('--button', '#BBB');
+    document.documentElement.style.setProperty('--text-color', '#BBB');
     document.documentElement.style.setProperty('--incorrect-color', 'rgb(180, 85, 85)');
     document.documentElement.style.setProperty('--background-color', '#444');
     document.documentElement.style.setProperty('--accent-color', '#F8EE33');
-    document.documentElement.style.setProperty('--header-background', '#444');
+    document.documentElement.style.setProperty('--header-background', '#333');
     document.documentElement.style.setProperty('--accent-light', '#FFFFF');
   }
 
-  OrangeClick(): void{
-    document.documentElement.style.setProperty('--button', '#000000');
-    document.documentElement.style.setProperty('--incorrect-color', '#656565');
-    document.documentElement.style.setProperty('--background-color', '#FF5F1A');
-    document.documentElement.style.setProperty('--accent-color', '#FFFFFF');
-    document.documentElement.style.setProperty('--header-background', '#4C86A8');
-    document.documentElement.style.setProperty('--accent-light', '#FFFFFF');
+  miamiClick(): void{
+    document.documentElement.style.setProperty('--button', '#f890e7');
+    document.documentElement.style.setProperty('--nav-button', '#f890e7');
+    document.documentElement.style.setProperty('--text-color', '#000000');
+    document.documentElement.style.setProperty('--incorrect-color', '#f890e7');
+    document.documentElement.style.setProperty('--background-color', '#ffffff');
+    document.documentElement.style.setProperty('--accent-color', '#0bd3d3');
+    document.documentElement.style.setProperty('--header-background', '#777');
+    document.documentElement.style.setProperty('--accent-light', '#eee');
+    document.documentElement.style.setProperty('--black-or-white', '#000000');
   }
 
   blueClick():void{
-    document.documentElement.style.setProperty('--button', '#000000');
+    document.documentElement.style.setProperty('--button', '#dcdcdc');
+    document.documentElement.style.setProperty('--nav-button', '#78A2CC');
+    document.documentElement.style.setProperty('--text-color', '#ffffff');
     document.documentElement.style.setProperty('--incorrect-color', 'rgb(180, 85, 85)');
-    document.documentElement.style.setProperty('--background-color', '#0CBABA');
-    document.documentElement.style.setProperty('--accent-color', '#FFFFFF');
-    document.documentElement.style.setProperty('--header-background', '#234AA6');
+    document.documentElement.style.setProperty('--background-color', '#78A2CC');
+    document.documentElement.style.setProperty('--accent-color', '#144B86');
+    document.documentElement.style.setProperty('--header-background', '#AECBD6');
+    document.documentElement.style.setProperty('--black-or-white', '#ffffff');
     document.documentElement.style.setProperty('--accent-light', '#000000');
   }
 
   lightClick():void{
     document.documentElement.style.setProperty('--button', '#000000');
+    document.documentElement.style.setProperty('--nav-button', '#eee');
     document.documentElement.style.setProperty('--incorrect-color', 'rgb(180, 85, 85)');
     document.documentElement.style.setProperty('--background-color', '#FFFFFF');
-    document.documentElement.style.setProperty('--accent-color', 'rgb(62, 227, 172');
-    document.documentElement.style.setProperty('--header-background', '#DDD');
+    document.documentElement.style.setProperty('--accent-color', 'rgb(62, 227, 172)');
+    document.documentElement.style.setProperty('--header-background', '#888');
+    document.documentElement.style.setProperty('--black-or-white', '#000000');
     document.documentElement.style.setProperty('--accent-light', '#000000');
   }
 
