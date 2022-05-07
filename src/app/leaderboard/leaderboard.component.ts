988247f9d4804067665435de6141e0d8eae14b15
benjaminrybacki <39493502+benjaminrybacki@users.noreply.github.com>
2022-05-06 21:43:23 -0500

    styled leaderboard page

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -10,29 +10,61 @@ import { UserService } from '../user.service';
 export class LeaderboardComponent implements OnInit {
 
   constructor(private userServ: UserService) { }
-  public listofUsers: User[]= [];
-  public topTen: User[] = [];
-  public topTwentyFive: User[] = [];
-  public topFifty: User[] = [];
+
+  listofUsers: User[] = [];
+  topTen: any[] = [];
+  topTwentyFive: any[] = [];
+  topFifty: any[] = [];
+  leaderboards: any[] = [];
+
   ngOnInit(): void {
-    this.getAllUsers();
-    
-    
+    this.getData();
+    document.querySelector('.ten')?.classList.add('active');
   }
-  getAllUsers(): void{
-    this.userServ.getUsers().subscribe(data =>{
-      this.listofUsers= data;
-      this.topTen = this.listofUsers;
-    this.topTwentyFive = this.listofUsers;
-    this.topFifty = this.listofUsers;
 
-    this.topTen.sort((a,b) => (b.TenWPM - a.TenWPM));
-    this.topTwentyFive.sort((a,b) => (b.TwentyFiveWPM - a.TwentyFiveWPM));
-    this.topFifty.sort((a,b) => (b.FiftyWPM - a.FiftyWPM));
+  getData(): void {
+    this.userServ.getUsers().subscribe(data => {
+      this.listofUsers = data;
+      this.topTen = this.listofUsers.map(({TenWPM, username}) => ({wpm: TenWPM, username }));
+      this.topTwentyFive = this.listofUsers.map(({TwentyFiveWPM, username}) => ({wpm: TwentyFiveWPM, username }));
+      this.topFifty = this.listofUsers.map(({FiftyWPM, username}) => ({wpm: FiftyWPM, username }));
+      console.log(this.topTen)
+      this.topTen.sort((a, b) => {
+        if(a.wpm < b.wpm) return -1;
+        if(b.wpm > a.wpm) return 1;
+        return 0;
+      });
+      this.topTwentyFive.sort((a, b) => {
+        if(a.wpm < b.wpm) return -1;
+        if(b.wpm > a.wpm) return 1;
+        return 0;
+      });
+      this.topFifty.sort((a, b) => {
+        if(a.wpm < b.wpm) return -1;
+        if(b.wpm > a.wpm) return 1;
+        return 0;
+      });
+      this.leaderboards = this.topTen;
     });
-
   }
 
+  lengthChange(words: number) : void {
+    if(words == 10) {
+      document.querySelectorAll('.test-length').forEach(element => element.classList.remove('active'));
+      document.querySelector('.ten')?.classList.add('active');
+      this.leaderboards = this.topTen;
+    }
+    else if (words == 25) {
+      document.querySelectorAll('.test-length').forEach(element => element.classList.remove('active'));
+      document.querySelector('.twenty-five')?.classList.add('active');
+      this.leaderboards = this.topTwentyFive;
+    }
+    else {
+      document.querySelectorAll('.test-length').forEach(element => element.classList.remove('active'));
+      document.querySelector('.fifty')?.classList.add('active');
+      this.leaderboards = this.topFifty;
+    }
 
+  }
 
 }
